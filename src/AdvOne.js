d1aefa4c2cc724f0ec8c65300df1a97fb65a7bf9
briannajzhang <briannagood800@gmail.com>
2021-11-10 15:56:23 -0600

    commit before merge

--- src/AdvOne.js
+++ src/AdvOne.js
@@ -1,6 +1,30 @@
 import './Style.css'
+import query from './api'
+import { useEffect, useState } from 'react';
+
 
 function AdvancedQueryOne() {
+    const [bestProfs, setBestProfs] = useState([]);
+
+    // get best professors
+    useEffect(() => {
+        var s = `SELECT professor, AVG(rating) AS avgRating 
+        FROM OfferingsToProfessorRelations opr JOIN Reviews r ON opr.id = r.offeringToProfessorRelation 
+        GROUP BY professor HAVING COUNT(rating) > 0 
+        ORDER BY avgRating DESC LIMIT 15;`;
+
+        query(s).then(res => {
+            let newBestProfs = res.map(elem => {
+                return <div class="p-4 flex space-x-4">
+                        <p className="w-100px">{elem[0]}</p>
+                        <p>{elem[1]}</p>
+                    </div>
+            });
+            setBestProfs(newBestProfs);
+            console.log(newBestProfs);
+        });
+    }, []);
+
     return <body>
     <header> 
         <span class="header_text">RateM</span>
@@ -8,7 +32,19 @@ function AdvancedQueryOne() {
         <span class="header_text">Professor</span>
     </header>
     <section class="body">
-        <div>This is the page for the first advanced query. Something will be returned here.</div>
+        <div>This is the page for the first advanced query, which returns top 15 best rated professors.</div>
+        <div>Click the button below to execute the query.</div>
+        
+        <div>
+        <button>EXECUTE QUERY HERE</button>
+        </div>
+
+        <div>
+            these are the best professors: 
+
+            {bestProfs}
+        </div>
+        
     </section>
 </body>
 }
--- src/Search_BACKUP_1165.js
+++ src/Search_BACKUP_1165.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import './Style.css'
-
-function Search() {
-    return <body>
-    <header> 
-        <span class="title_text">RateM</span>
-        <img class="title_image" src="http://ansatuiuc.web.engr.illinois.edu/images/Illinois_Block_I.png" alt="Block I" height="110" width="110"></img>
-        <span class="title_text">Professor</span>
-    </header>
-    <section class="body">
-        <div>This is the page for the first advanced query. Something will be returned here.</div>
-    </section>
-    </body>
-=======
 import { useEffect, useState } from "react";
 import query from "./api";
 
@@ -130,7 +115,6 @@ function Search() {
         </div>
     </div>
 
->>>>>>> e3219835794463be82980d6eb5e49a15e52bbfbe
 }
 
 export default Search;
